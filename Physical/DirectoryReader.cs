4a3f9abecec690a6fdc649edab17f87f229099c4
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-06 18:00:35 +0800

    建立 DirectoryReader
    1.　加入多載方法：GetFiles
    2.　加入方法：GetDirectories
    3.　加入多載方法：GetAllFiles

---
+++ Physical/DirectoryReader.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Physical
+{
+    public sealed class DirectoryReader
+    {
+        public List<string> GetFiles(string path)
+        {
+            return Directory.GetFiles(path).ToList();
+        }
+
+        public List<string> GetFiles(string path, string searchPattern)
+        {
+            return Directory.GetFiles(path, searchPattern).ToList();
+        }
+
+        public List<string> GetDirectories(string path)
+        {
+            return Directory.GetDirectories(path).ToList();
+        }
+
+        public List<string> GetAllFiles(string path)
+        {
+            var files = this.GetFiles(path);
+
+            foreach (var directory in this.GetDirectories(path))
+            {
+                var subDirectoryFiles = this.GetAllFiles(directory);
+
+                files.AddRange(subDirectoryFiles);
+            }
+
+            return files;
+        }
+
+        public List<string> GetAllFiles(string path, string searchPattern)
+        {
+            var files = this.GetFiles(path, searchPattern);
+
+            foreach (var directory in this.GetDirectories(path))
+            {
+                var subDirectoryFiles = this.GetAllFiles(directory, searchPattern);
+
+                files.AddRange(subDirectoryFiles);
+            }
+
+            return files;
+        }
+    }
+}
