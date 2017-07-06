8ff13aebad58622d151ea6329b9e689fa13b6716
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-06 18:08:12 +0800

    調整 DirectoryReader
    1.　公開多載方法：GetFiles
    2.　移除方法：GetAllFiles

--- Physical/DirectoryReader.cs
+++ Physical/DirectoryReader.cs
@@ -16,7 +16,7 @@ namespace Physical
             return Directory.GetFiles(path, searchPattern).ToList();
         }
 
-        private List<string> GetFiles(string path, string searchPattern, SearchOption searchOption)
+        public List<string> GetFiles(string path, string searchPattern, SearchOption searchOption)
         {
             return Directory.GetFiles(path, searchPattern, searchOption).ToList();
         }
@@ -24,16 +24,6 @@ namespace Physical
         public List<string> GetDirectories(string path)
         {
             return Directory.GetDirectories(path).ToList();
-        }
-
-        public List<string> GetAllFiles(string path)
-        {
-            return this.GetFiles(path, "*", SearchOption.AllDirectories);
-        }
-
-        public List<string> GetAllFiles(string path, string searchPattern)
-        {
-            return this.GetFiles(path, searchPattern, SearchOption.AllDirectories);
         }
     }
 }
