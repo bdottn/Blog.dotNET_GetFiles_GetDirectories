60350a1a179f241428f40a0fb18fd81b972a2567
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-06 18:07:44 +0800

    調整 DirectoryReader
    1.　加入多載方法：GetFiles
    2.　調整方法：GetAllFiles

--- Physical/DirectoryReader.cs
+++ Physical/DirectoryReader.cs
@@ -16,6 +16,11 @@ namespace Physical
             return Directory.GetFiles(path, searchPattern).ToList();
         }
 
+        private List<string> GetFiles(string path, string searchPattern, SearchOption searchOption)
+        {
+            return Directory.GetFiles(path, searchPattern, searchOption).ToList();
+        }
+
         public List<string> GetDirectories(string path)
         {
             return Directory.GetDirectories(path).ToList();
@@ -23,30 +28,12 @@ namespace Physical
 
         public List<string> GetAllFiles(string path)
         {
-            var files = this.GetFiles(path);
-
-            foreach (var directory in this.GetDirectories(path))
-            {
-                var subDirectoryFiles = this.GetAllFiles(directory);
-
-                files.AddRange(subDirectoryFiles);
-            }
-
-            return files;
+            return this.GetFiles(path, "*", SearchOption.AllDirectories);
         }
 
         public List<string> GetAllFiles(string path, string searchPattern)
         {
-            var files = this.GetFiles(path, searchPattern);
-
-            foreach (var directory in this.GetDirectories(path))
-            {
-                var subDirectoryFiles = this.GetAllFiles(directory, searchPattern);
-
-                files.AddRange(subDirectoryFiles);
-            }
-
-            return files;
+            return this.GetFiles(path, searchPattern, SearchOption.AllDirectories);
         }
     }
 }
