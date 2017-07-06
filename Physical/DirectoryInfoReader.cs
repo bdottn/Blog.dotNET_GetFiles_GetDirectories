5c17bbdeaaf560c27c09317aafa66f2567c8e9c3
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-06 18:14:19 +0800

    建立 DirectoryInfoReader
    1.　加入多載方法：GetFiles
    2.　加入多載方法：GetDirectories

---
+++ Physical/DirectoryInfoReader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Physical
+{
+    public sealed class DirectoryInfoReader
+    {
+        public List<FileInfo> GetFiles(string path)
+        {
+            return new DirectoryInfo(path).GetFiles().ToList();
+        }
+
+        public List<FileInfo> GetFiles(string path, string searchPattern)
+        {
+            return new DirectoryInfo(path).GetFiles(searchPattern).ToList();
+        }
+
+        public List<FileInfo> GetFiles(string path, string searchPattern, SearchOption searchOption)
+        {
+            return new DirectoryInfo(path).GetFiles(searchPattern, searchOption).ToList();
+        }
+
+        public List<DirectoryInfo> GetDirectories(string path)
+        {
+            return new DirectoryInfo(path).GetDirectories().ToList();
+        }
+
+        public List<DirectoryInfo> GetDirectories(string path, string searchPattern)
+        {
+            return new DirectoryInfo(path).GetDirectories(searchPattern).ToList();
+        }
+
+        public List<DirectoryInfo> GetDirectories(string path, string searchPattern, SearchOption searchOption)
+        {
+            return new DirectoryInfo(path).GetDirectories(searchPattern, searchOption).ToList();
+        }
+    }
+}
